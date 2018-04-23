43229867d7c906b5e4087f2a3623ff360a38bf1c
GermanJackie <jbast@buffalo.edu>
2018-04-23 11:53:41 -0400

    Arduino & Unity code
    Arduino Code & Unity code for Wand

--- Homunculus/Assets/VibSense.cs
+++ Homunculus/Assets/VibSense.cs
@@ -1,40 +1,44 @@
-﻿using System.Collections;
+﻿//IMPORTANT: Edit - Project Settings - Player - Other Settings 
+//-> Configuration - Api Compatibility must be set to .NET 2.0
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
 
-public class VibSense : MonoBehaviour {
+public class VibSense : MonoBehaviour
+{
 
-	SerialPort stream = new SerialPort("/dev/cu.usbmodem1411", 9600);
-	int vibState = 0;
+	SerialPort stream = new SerialPort ("/dev/cu.usbmodem1411", 9600); //check port name in Arduino and match!
 
-
-	void Start () {
-        stream.Open();
-        stream.ReadTimeout = 50;
-	}
-
-
-	void Update () {
-        if (stream.IsOpen)
-        {
-            string value = stream.ReadLine();
-            vibState = int.Parse(value);
-            Debug.Log(value);
-        }
-	}
-
-
-
-	/* void OnGUI()
+	void Start ()
 	{
-		string newString = "Connected: " + vibState;
-		//Debug.Log (vibState);
-		//OnGUI.Label (new Rect (10, 10, 300, 100), newString);
+		stream.Open ();
+		stream.ReadTimeout = 1;
 	}
-	*/
 
-	//void Wand() {
 
+	void Update ()
+	{
+
+		if (stream.IsOpen) {
+			try {
+				Wand (stream.ReadByte ());
+			} catch (System.Exception) {
+			}
+
+		}
+	}
+
+
+	void Wand (int vibState)
+	{
+	
+		if (vibState == 0) { 
+			//MAKE SPARKLES...
+			int value = stream.ReadByte (); //read Arduino byte and store value as integer
+			//if arduino sends 0 (triggered), Unity receives 48 (byte representation of character 0), so use 48 to trigger events 
+			print(value); //check in console
+		}
 }
-
+}
